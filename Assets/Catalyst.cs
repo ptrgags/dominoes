8b3450b39e92c272d7fc9d5ca8ebafac45fbc6fe
Peter Gagliardi <ptrgags@live.com>
2016-09-13 21:38:37 -0400

    Simple example of dominoes
    Added a simple example of falling dominoes.

---
+++ Assets/Catalyst.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Catalyst : MonoBehaviour {
+	public float thrust = 0;
+	public Rigidbody body;
+
+	void Start() {
+		body = GetComponent<Rigidbody>();
+		body.AddForce(0, 0, thrust);
+	}
+}
