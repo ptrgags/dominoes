cdef4dd9c155256ba6fe16917cac6ffd7de861c6
Peter Gagliardi <ptrgags@live.com>
2016-09-21 20:33:11 -0400

    Fix Domino Chain Gizmos
    Draw better Domino Chain gizmos. now I can draw rotated boxes.
    
    Also commented the code so I understand what's going on.

--- Assets/DominoChain.cs
+++ Assets/DominoChain.cs
@@ -1,35 +1,75 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * Chain of dominoes in a straight line
+ */
 public class DominoChain : MonoBehaviour {
+	/** The prefab that represents a Domino */
 	public GameObject domino_prefab;
+	/** Number of dominoes in the chain */
 	public int num_dominoes = 10;
+	/** Spacing between each dominoes */
 	public float spacing = 0.2f;
 
 	// Use this for initialization
 	void Start () {
+		//The first domino's center is the same as the parent
 		Vector3 first_domino_center = gameObject.transform.position;
+
+		//Each domino is rotated the same as the parent
 		Quaternion rotation = gameObject.transform.rotation;
+
+		//Create dominoes in a straight line
 		for (int i = 0; i < num_dominoes; i++) {
-			Vector3 pos = new Vector3 (i * spacing, 0, 0);
+			//Create a new child GameObject from the Domino prefab.
+			Vector3 pos = new Vector3(i * spacing, 0, 0);
 			GameObject obj = (GameObject)Instantiate(domino_prefab, first_domino_center + rotation * pos, rotation);
 			obj.transform.parent = gameObject.transform;
 		}
 	}
 
 	void OnDrawGizmos() {
+		//The first domino's center is the same as the parent
 		Vector3 first_domino_center = gameObject.transform.position;
+
+		//All dominoes are rotated the same as the parent
 		Quaternion rotation = gameObject.transform.rotation;
-		Vector3 last_offset = new Vector3 ((num_dominoes - 1) * spacing, 0, 0);
-		Vector3 last_domino_center = first_domino_center + rotation * last_offset;
+
+		//Get the domino dimensions from the prefab
+		Vector3 domino_dims = domino_prefab.transform.localScale;
+
+		//Vector that represents the distance between each pair of dominoes
+		Vector3 delta = rotation * (new Vector3(spacing, 0, 0));
+
+		//Get the position of the second and last dominoes
+		Vector3 second_domino_center = first_domino_center + delta;
+		Vector3 last_domino_center = first_domino_center + (num_dominoes - 1) * delta;
+
+		//Draw the first, second and last dominoes to show the length of the domino
+		//chain and the spacing.
 		Gizmos.color = Color.red;
-		Gizmos.DrawSphere (first_domino_center, 0.1f);
-		Gizmos.DrawLine (first_domino_center, last_domino_center);
-		Gizmos.DrawSphere (last_domino_center, 0.1f);
+		DrawBox(first_domino_center, rotation, domino_dims);
+		DrawBox(second_domino_center, rotation, domino_dims);
+		DrawBox(last_domino_center, rotation, domino_dims);
+		Gizmos.DrawLine(first_domino_center, last_domino_center);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	/**
+	 * Draw a wireframe box Gizmo with rotation
+	 */
+	void DrawBox(Vector3 position, Quaternion rotation, Vector3 scale) {
+		//Save the old matrix
+		Matrix4x4 old_matrix = Gizmos.matrix;
+
+		//Add a transformation to the graphics context
+		Matrix4x4 new_matrix = Matrix4x4.TRS(position, rotation, scale);
+		Gizmos.matrix *= new_matrix;
+
+		//Draw the wireframe box
+		Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+
+		//Restore the old matrix
+		Gizmos.matrix = old_matrix;
 	}
 }
