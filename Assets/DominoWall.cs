b45a79220450142123e9486b9f9388e423d9c391
Peter Gagliardi <ptrgags@live.com>
2016-09-26 19:54:17 -0400

    Refactor Domino Wall to be a DominoStructure

--- Assets/DominoWall.cs
+++ Assets/DominoWall.cs
@@ -1,89 +1,88 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DominoWall : MonoBehaviour {
-    //Prefab for the basic domino in this wall
-    public GameObject domino_prefab;
-    /** The material for coloring the dominoes in this chain */
-    public Material domino_material;
+public class DominoWall : DominoStructure {
     /** Number of dominoes along the ground */
     public int num_dominoes = 10;
-    /** Number of layers in the wall */
-    public int layers = 3;
+
+    private Quaternion ground_rotation = Quaternion.Euler(90, 0, 0);
+    private Quaternion wall_rotation = Quaternion.Euler(90, 90, 0);
+
+    private Vector3 ground_spacing_delta {
+        get {
+            return new Vector3(
+                domino_template.Height - domino_template.Thickness,
+                0,
+                0);
+        }
+    }
+
+    private Vector3 half_layer_delta {
+        get {
+            return new Vector3(0, 2 * domino_template.Width, 0);
+        }
+    }
+
+    private Vector3 first_wall_offset {
+        get {
+            return new Vector3(
+                domino_template.HalfHeight - domino_template.HalfThickness, 
+                domino_template.Width, 
+                0);
+        }
+    }
+
+    private Vector3 first_wall_offset2 {
+        get {
+            return new Vector3(
+                domino_template.Height - domino_template.Thickness 
+                + domino_template.HalfHeight - domino_template.HalfThickness, 
+                domino_template.Width, 
+                0);
+        }
+    }
+
+    private Vector3 wall_spacing_delta {
+        get {
+            return new Vector3(
+                2 * (domino_template.Height - domino_template.Thickness), 
+                0, 
+                0);
+        }
+    }
+
+    private Vector3 center_to_side_delta {
+        get {
+            return new Vector3(
+                0, 
+                0, 
+                domino_template.HalfHeight - domino_template.HalfThickness);
+        }
+    }
 
 	// Use this for initialization
 	void Start () {
-        //The first domino's center is the same as the parent
-        Vector3 first_domino_center = gameObject.transform.position;
-
-        //Dominoes along the ground need to be rotated 90 degrees
-        Quaternion ground_rotation = Quaternion.Euler(90, 0, 0);
-
-        Domino domino_template = domino_prefab.GetComponent<Domino>();
-        float ground_spacing = domino_template.Height - domino_template.Thickness;
-
-        Vector3 layer_delta = new Vector3(0, 2.0f * domino_template.Width, 0);
-
-        for(int i = 0; i < layers + 1; i++) {
-            for(int j = 0; j < num_dominoes; j++) {
-                Vector3 pos = new Vector3(j * ground_spacing, 0, 0);
-                GameObject domino = (GameObject)Instantiate(domino_prefab, i * layer_delta + first_domino_center + pos, ground_rotation);
-
-                //Mark the domino as a child of the domino wall
-                domino.transform.parent = gameObject.transform;
-
-                //set the child domino's material to match the one specified 
-                //for the chain.
-                domino.GetComponent<Renderer>().material = domino_material;
-            }
+        //Horizontal dominoes
+        for (int i = 0; i < num_dominoes; i++) {
+            Vector3 pos = i * ground_spacing_delta;
+            add_domino(pos, ground_rotation);
+            add_domino(pos + half_layer_delta, ground_rotation);
         }
 
-        float wall_spacing = 2.0f * domino_template.Height - 2.0f * domino_template.Thickness;
-        Quaternion wall_rotation = Quaternion.Euler(90, 90, 0);
-        float first_domino_offset = domino_template.HalfHeight - domino_template.HalfThickness;
-        float width_offset = domino_template.HalfHeight - domino_template.HalfThickness;
-
-        for (int i = 0; i < layers; i++) {
-            for(int j = 0; j < num_dominoes / 2; j++) {
-                Vector3 pos = new Vector3(j * wall_spacing + first_domino_offset, domino_template.Width, width_offset);
-                GameObject domino = (GameObject)Instantiate(domino_prefab, i * layer_delta + first_domino_center + pos, wall_rotation);
-
-                //Mark the domino as a child of the domino wall
-                domino.transform.parent = gameObject.transform;
-
-                //set the child domino's material to match the one specified 
-                //for the chain.
-                domino.GetComponent<Renderer>().material = domino_material;
-            }
+        //Right wall in first half, left wall in second half
+        for (int i = 0; i < num_dominoes / 2; i++) {
+            Vector3 pos = i * wall_spacing_delta + first_wall_offset;
+            add_domino(pos + center_to_side_delta, wall_rotation);
+            add_domino(pos - center_to_side_delta + half_layer_delta, wall_rotation);
         }
 
-        first_domino_offset = domino_template.Height + domino_template.HalfHeight - domino_template.Thickness - domino_template.HalfThickness;
-
-        for (int i = 0; i < layers; i++) {
-            for (int j = 0; j < (num_dominoes - 1) / 2; j++) {
-                Vector3 pos = new Vector3(j * wall_spacing + first_domino_offset, domino_template.Width, -width_offset);
-                GameObject domino = (GameObject)Instantiate(domino_prefab, i * layer_delta + first_domino_center + pos, wall_rotation);
-
-                //Mark the domino as a child of the domino wall
-                domino.transform.parent = gameObject.transform;
-
-                //set the child domino's material to match the one specified 
-                //for the chain.
-                domino.GetComponent<Renderer>().material = domino_material;
-            }
+        //Right wall in second half, left wall in first half
+        for (int i = 0; i < (num_dominoes - 1) / 2; i++) {
+            Vector3 pos = i * wall_spacing_delta + first_wall_offset2;
+            add_domino(pos - center_to_side_delta, wall_rotation);
+            add_domino(pos + center_to_side_delta + half_layer_delta, wall_rotation);
         }
-           
-        for(int i = 0; i < layers; i++) {
-            Vector3 pos = new Vector3(0, domino_template.HalfWidth + domino_template.HalfHeight, domino_template.HalfWidth);
-            GameObject domino = (GameObject)Instantiate(domino_prefab, i * layer_delta + first_domino_center + pos, wall_rotation);
-
-            //Mark the domino as a child of the domino wall
-            domino.transform.parent = gameObject.transform;
-
-            //set the child domino's material to match the one specified 
-            //for the chain.
-            domino.GetComponent<Renderer>().material = domino_material;
-        }
+        //Debug.Break();
 	}
 	
     void OnDrawGizmos() {
